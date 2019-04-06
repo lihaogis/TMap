b72d272435acb004ee4c45307e31bcebf70888c0
unknown <645015594@qq.com>
2019-04-06 23:01:07 +0800

    gulp

---
+++ dist/js/tmap.js
@@ -0,0 +1,1 @@
+ol.source.AbcSLMap=function(e){return new ol.source.XYZ({url:"http://webrd03.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8"})},ol.source.AbcYXMap=function(e){return new ol.source.XYZ({url:"http://webrd03.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8"})};
