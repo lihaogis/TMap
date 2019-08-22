cd539a4a8f56e570a43df23912d1324d580999d8
lhgis <645015597@qq.com>
2019-08-22 22:43:40 +0800

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require("gulp");
 var concat = require("gulp-concat");
 // 获取 uglify 模块（用于压缩 JS）
 var uglify = require('gulp-uglify');
+var obfuscate = require('gulp-obfuscate');
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
 gulp.task('jscompress', function() {
@@ -10,6 +11,7 @@ gulp.task('jscompress', function() {
    return gulp.src('src/source/TMap.js')
     // 2. 压缩文件
         .pipe(uglify())
+        .pipe(obfuscate())
         // 3. 另存压缩后的文件
 		.pipe(concat('tmap'+".js"))
         .pipe(gulp.dest('dist/js'));
